c9ce3736792e01d2cd11bdd17f7210f0dc64f55c
Carl Keneth <baseleres.ck@gmail.com>
2019-04-20 08:15:24 +0800

    fixed fetch for sign in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -28,8 +28,9 @@ class signIn extends React.Component{
       })
         .then(response => response.json())
         .then(data => {
-          if (data === 'success') {
-            this.props.onRouteChange('home')
+          if (data.id) {
+            this.props.loadUser(data);
+            this.props.onRouteChange('home');
           }
         })
    }
